72f9becdd945b506acc4b89d250723eed5480750
gracious <gracepopoola2022@gmail.com>
2025-01-22 10:02:28 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}",
+    "./index.html",
+  ],
+  theme: {
+    extend: {
+      // height:{
+      //   65:'65%',
+      // }
+    },
+  },
+  plugins: [],
+}
+
